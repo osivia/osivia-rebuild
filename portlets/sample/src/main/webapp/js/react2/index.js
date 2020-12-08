04d0d033ece51a79fa9b2b14dff1f71c5b42570d
Jean-Sébastien <jssteux@osivia.com>
2020-12-08 10:26:28 +0100

    react apres npm build

---
+++ portlets/sample/src/main/webapp/js/react2/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./components/Game";
+
+ReactDOM.render(React.createElement(Game, null), document.getElementById("root"));
---
+++ portlets/sample/src/main/webapp/js/react2/precache-manifest.88361ffc32bc78c8d0f80ebbe33bdd8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b256beafeee14794fd82a298782243",
+    "url": "/react-tutorial-solutions/index.html"
+  },
+  {
+    "revision": "f773b7a8688aac48c25c",
+    "url": "/react-tutorial-solutions/static/css/main.a6b8b413.chunk.css"
+  },
+  {
+    "revision": "ff2e545317823c49b495",
+    "url": "/react-tutorial-solutions/static/js/2.da35e365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial-solutions/static/js/2.da35e365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f773b7a8688aac48c25c",
+    "url": "/react-tutorial-solutions/static/js/main.d822d3aa.chunk.js"
+  },
+  {
+    "revision": "7a13090e4d07042daf3b",
+    "url": "/react-tutorial-solutions/static/js/runtime-main.d04aa953.js"
+  }
+]);
