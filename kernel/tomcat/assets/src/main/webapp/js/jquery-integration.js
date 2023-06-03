2bf8d083ed026b99225b9181a9ec4919c90e80b7
jssteux <jssteux@osivia.org>
2023-06-03 17:44:53 +0200

    improved js validation

--- kernel/tomcat/assets/src/main/webapp/js/jquery-integration.js
+++ kernel/tomcat/assets/src/main/webapp/js/jquery-integration.js
@@ -23,35 +23,80 @@ var $JQry = jQuery.noConflict();
 
 
 // bs5 validation
+
+
+function updateFeedback( element) {
+  
+       var $element = $JQry(element);
+       $element.closest('.form-group').find('.invalid-feedback').each(function(index, msg){
+           
+           var message = element.validationMessage;
+               
+           // Don't display required message (style is sufficient)
+            if($element.val() == "")
+                message = "";
+                                                  
+            if($element.data("error") != null)
+                  message = $element.data("error");
+                    
+             var $msg = $JQry(msg);
+             $msg.text( message);
+       });
+   
+}
+
+
+function fieldValidation( field) {
+
+    if(field.validationMessage)   {
+        updateFeedback( field);
+    }
+    // Force the display (bootstrap css rule)
+    field.parentElement.classList.add('was-validated')  
+
+}
+
+
+
 $JQry(function() {
     $JQry('.needs-validation').each(function(index, element) {
         var $element = $JQry(element),
             loaded = $element.data("validation-loaded");
 
         if (!loaded) {
-            
+             $element.find(':input').on('keyup change', function () {
+
+             window.setTimeout( fieldValidation ,              
+                                500,
+                                this);
+                 
+
+            });
+                
             // prevent submission
             element.addEventListener('submit', function(event) {
                 if (!element.checkValidity()) {
+                    
+                    var focusField = null;
  
-                    // display feedback                   
+                    // display feedback on submit                  
                     $element.find(':input').each(function(index, child){
+                        
                          if( child.validationMessage)    {
-                               var $child = $JQry(child);
-                               $child.closest('.form-group').find('.invalid-feedback').each(function(index, msg){
-                                   
-                                   var message = child.validationMessage;
-                                   // Don't display required message (style is sufficient)
-                                   if($child.val() == "")
-                                        message = "";
-                                   var $msg = $JQry(msg);
-                                   $msg.text( message);
-                               });
+                              updateFeedback( child);
+                               
+                          if( focusField == null)
+                                focusField = child;
                         }
                     });
                     
+                   
                     event.preventDefault()
                     event.stopPropagation()
+                    
+                    if( focusField != null) {
+                        focusField.focus();
+                    }
                 }
 
                 element.classList.add('was-validated')
